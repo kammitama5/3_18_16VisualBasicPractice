61d597820830da809791b6cf0234b1fd22dc83b8
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:00:16 -0700

    next one: using DIM
    simple addition

---
+++ NewProgramme1/NewProgramme1/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim x As Integer
+        Dim y As Integer
+
+        x = 7
+        y = x + 3
+        Console.WriteLine(y)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
