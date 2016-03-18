7f2033a061fd9ef72eaa3f4dbb96e7205bed2b4b
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:10:15 -0700

    function basic
    function basic

---
+++ MyName/MyName/MyName1.vb
@@ -0,0 +1,13 @@
+﻿Module MyName1
+
+    Sub Main()
+
+        Dim myFirstName As String = "Krystal"
+
+
+        Console.WriteLine(myFirstName)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
