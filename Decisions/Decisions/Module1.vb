940af4a0102b7cd8e2b52be602f35494c545c548
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:36:55 -0700

    decisions

---
+++ Decisions/Decisions/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("Please type something and press enter")
+        Dim userValue As String
+        userValue = Console.ReadLine()
+        Console.WriteLine("You typed " & userValue)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
