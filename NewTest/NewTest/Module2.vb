6ed86a13c4e1eb2c749d24a2bbee90e2bb931ec9
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:23:50 -0700

    toUpper
    toUpper

---
+++ NewTest/NewTest/Module2.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim x As Integer = 7
+        Dim y As String = "Krystal"
+        Dim spaceString As String = " "
+        Dim myFirstTry As String = x & spaceString & y.ToUpper
+
+
+
+
+        Console.WriteLine(myFirstTry)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
