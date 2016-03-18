7267c3015c2c23a388fa59656a1071c5b222c9c1
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:49:48 -0700

    decisions with a twist

---
+++ Decisions/Decisions/Module2.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Console.WriteLine("Please type something and press enter")
+        'Dim userValue As String
+        'userValue = Console.ReadLine()
+        'Console.WriteLine("You typed " & userValue)
+        'Console.ReadLine()
+
+        Console.WriteLine("Would you prefer what is behind door number 1, 2 or 3? ")
+        Dim userValue As String = Console.ReadLine()
+        If userValue = "1" Then
+            Console.Write("You are now a billionnaire!")
+            Console.ReadLine()
+        ElseIf userValue = "2" Then
+            Console.Write("You won a yacht!")
+            Console.ReadLine()
+        Else
+            Console.Write("You ain't won shit....")
+            Console.ReadLine()
+        End If
+
+
+    End Sub
+
+End Module
