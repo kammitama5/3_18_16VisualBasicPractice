8571f0a0e98a8050f0e05c044b2d5fc429dcf1ef
kammitama5 <krystal.maughan@gmail.com>
2016-03-18 14:20:36 -0700

    concat and vars
    concat and vars

---
+++ NewTest/NewTest/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim x As Integer = 7
+        Dim y As String = "Krystal"
+        Dim spaceString As String = " "
+        Dim myFirstTry As String = x & spaceString & y
+
+
+
+        Console.WriteLine(myFirstTry)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
